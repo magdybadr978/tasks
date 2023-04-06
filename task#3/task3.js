e027e1ee20f87e0b24ccfab0ab2b3f6dbb9ae3d0
magdybadr978 <119052252+magdybadr978@users.noreply.github.com>
2023-04-06 22:47:52 +0200

    Update task3.js

--- task#3/task3.js
+++ task#3/task3.js
@@ -1,72 +1,102 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-const { v4 } = require('uuid');
-const router = require("express").Router();
-const teams = [];
-app.use("/teams" , teams);
-
+const fs = require("fs");
 
 // GET Request
-router.get('/' , (req,res) =>{
-    console.log(req);
-    res.send(teams);
-})
-
-
-// POST Request  -> save data
-router.post('/' ,(req,res) =>{
-    const data = req.body;
-    teams.push({
-        id :  v4(),
-        name : data.name,
-        trophies : data.trophies,
-        points : data.points,
-    });
-    res.send({
-        message : "team created",
-    });
-})
-
-// PUT Request -> update data
-router.put('/:id' ,(req,res) => {
-    const { id } = req.params;
-    const data = req.body;
-    const teamIndex = movies.findIndex((item) => item.id == id);
-    if(teamIndex == -1){
-        res.statusCode = 404;
-        res.send({
-            message : "team not found",
-        });
-    }else{
-        teams[teamIndex].name = data.name;
-        teams[teamIndex].trophies = data.trophies;
-        teams[teamIndex].points = data.points;
-        res.json(teams[teamIndex]);
+app.get("/teams", (req, res) => {
+  fs.readFile("teams.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
     }
+    res.status(200).send(data);
+  });
+});
+
+// PATCH Request  -> Update data
+app.patch("/teams/:id", (req, res) => {
+  const { id } = req.params;
+  fs.readFile("teams.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const information = JSON.parse(data);
+    const teamIndex = information.findIndex((team) => team.id === id);
+    if (teamIndex === -1) {
+      res.status(404).send({
+        message: "team not found",
+      });
+      return;
+    }
+    // information[teamIndex].id = req.body.id;
+    // information[teamIndex].name = req.body.name;
+    // information[teamIndex].trophies = req.body.trophies;
+    // information[teamIndex].points = req.body.points;
+
+    information[teamIndex] = {...information[teamIndex],...req.body};
+    fs.writeFile("teams.json", JSON.stringify(information), (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.status(200).send({
+        message: "data updated successfully",
+      });
+    });
+  });
+});
+
+app.post("/teams", (req, res) => {
+  fs.readFile("teams.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const information = JSON.parse(data);
+    let newData = req.body;
+    information.push(newData);
+    fs.writeFile("teams.json", JSON.stringify(information), (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.send("data added successful");
+    });
+  });
 });
 
 // DELETE Request -> remove specific team
-router.delete('/:id' ,(req,res) =>{
-    const { id } = req.params;
-    const teamIndex = teams.findIndex((item) => item.id == id);
-    if(teamIndex == -1){
-        res.statusCode = 404;
-        res.send({
-            message : "team not found",
-        });
-    }else{
-        teams.splice(teamIndex , 1);
-        res.json({
-            message : "team Deleted",
-        })
+app.delete("/teams/:id", (req, res) => {
+  const { id } = req.params;
+  fs.readFile("teams.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send({
+        message: "err reading file",
+      });
+      return;
     }
+    const teams = JSON.parse(data);
+    const teamIndex = teams.findIndex((team) => team.id === id);
+
+    if (teamIndex === -1) {
+      res.status(404).send({
+        message: "team not found",
+      });
+    }
+    teams.splice(teamIndex, 1);
+    fs.writeFile("teams.json", JSON.stringify(teams), (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    });
+    res.status(200).send({
+      message: "Team deleted",
+    });
+  });
 });
-
-
-
-
-
-app.listen(4000 , "localhost" ,() =>{
-    console.log("server is running");
-})
+app.listen(4000, "localhost", () => {
+  console.log("server is running");
+});
